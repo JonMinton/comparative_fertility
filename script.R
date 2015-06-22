c807dc5643fb50c1c579bad7918a7ed4c19afd6a
Jonathan Minton <nate.minton@gmail.com>
2015-06-22 23:34:09 +0100

    create code for ccfr

---
+++ script.R
@@ -0,0 +1,173 @@
+
+rm(list=ls())
+
+require(readr)
+
+require(plyr)
+require(tidyr)
+require(stringr)
+require(dplyr)
+
+require(ggplot2)
+require(lattice)
+require(latticeExtra)
+
+
+require(scales)
+
+
+dta <- read_csv("data/lexis_square_combined.csv")
+
+
+
+dta <- dta %>% 
+  mutate(birth_year = year - age) %>% 
+  mutate(my_asfr = total / exposure) %>% 
+  group_by(code, year) %>% 
+  mutate(my_cpfr = lag(cumsum(total/exposure), 1)) %>% 
+  ungroup 
+
+dta <- dta  %>% 
+  arrange(code, birth_year, age) %>%  
+  group_by(code, birth_year) %>%  
+  mutate(my_ccfr = lag(cumsum(total/exposure), 1)) %>% 
+  ungroup
+
+
+
+dir.create("figures/asfr/", recursive = TRUE)
+dir.create("figures/cpfr/", recursive = TRUE)
+dir.create("figures/ccfr/", recursive = TRUE)
+
+
+spool_asfr_figs <- function(x){
+  this_country <- x$code[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "asfr_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hfd/asfr/", file_label, ".png"),
+      res=300, width=25, height=25, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 50 ) %>% 
+    contourplot(
+      my_asfr ~ year * age , 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Year", cex=1.4),
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+      main=NULL,
+      labels=list(cex=1.2),
+      col="black",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta, .(code), spool_asfr_figs, .progress="text")
+
+
+spool_cpfr_figs <- function(x){
+  this_country <- x$code[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "cpfr_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/hfd/cpfr/", file_label, ".png"),
+      res=300, width=25, height=25, units = "cm"
+  )
+  
+  
+  
+  p <- x %>% filter( age <= 50 ) %>% 
+    contourplot(
+      my_cpfr ~ year * age , 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Year", cex=1.4),
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+      main=NULL,
+      labels=list(cex=1.2),
+      col="black",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  print(p)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta_hfd, .(code), spool_cpfr_figs, .progress="text")
+
+
+
+spool_ccfr_figs <- function(x){
+  
+  
+  p <- x %>% filter( age <= 50 ) %>% 
+    contourplot(
+      my_ccfr ~ birth_year * age , 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Birth year", cex=1.4),
+      main = x$code[1],
+      cex=1.4,
+      cuts=20,
+      col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+      labels=list(cex=1.2),
+      col="black",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  return(p)
+}
+
+
+tmp <- dlply(dta, .(code), spool_ccfr_figs, .progress="text")
+
