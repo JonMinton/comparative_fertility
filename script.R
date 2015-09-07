51f699bf57d6a8fb3077203e0e3210df1f2e22f8
JonMinton <nate.minton@gmail.com>
2015-09-07 15:18:42 +0100

    added flipped ccfr figs

--- script.R
+++ script.R
@@ -48,6 +48,8 @@ dta <- dta %>% left_join(selector)
 dir.create("figures/asfr/", recursive = TRUE)
 dir.create("figures/cpfr/", recursive = TRUE)
 dir.create("figures/ccfr/", recursive = TRUE)
+dir.create("figures/flipped_ccfr/", recursive = TRUE)
+
 
 
 spool_asfr_figs <- function(x){
@@ -223,6 +225,77 @@ d_ply(dta, .(code), spool_ccfr_figs, .progress="text")
 
 
 
+spool_flipped_ccfr_figs <- function(x){
+  this_country <- x$code[1]
+  min_year <- min(x$year)
+  max_year <- max(x$year)
+  
+  title_label <- paste0(
+    this_country, " (", min_year, " - ", max_year, ")"
+  )
+  
+  file_label <- paste0(
+    "ccfr_", this_country, "_(", min_year, "_", max_year, ")" 
+  )
+  
+  png(paste0("figures/flipped_ccfr/", file_label, ".png"),
+      res=300, width=25, height=25, units = "cm"
+  )
+  
+  
+  
+  p1 <- x %>% filter( age <= 50 ) %>% 
+    levelplot(
+      my_asfr ~ age * birth_year , 
+      data=. , 
+      region=T, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab=list(label="Age in years", cex=1.4),
+      xlab=list(label="Birth year", cex=1.4),
+      main = x$code[1],
+      cex=1.4,
+      col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  p2 <- x %>% 
+    filter( age <= 50 ) %>% 
+    filter(series_ok == TRUE) %>% 
+    contourplot(
+      my_ccfr ~ age * birth_year , 
+      data=. , 
+      region=F, 
+      par.strip.text=list(cex=1.4, fontface="bold"),
+      ylab="",
+      xlab="",
+      main ="",
+      label.style = "align",
+      at = seq(from = 0.5, to = 2.5, by = 0.1 ),
+      labels=list(cex=1.2),
+      col="black",
+      scales=list(
+        x=list(cex=1.4), 
+        y=list(cex=1.4),
+        alternating=3
+      )
+    )
+  
+  
+  print(p1 + p2)
+  dev.off()
+  
+  return(NULL)
+}
+
+
+d_ply(dta, .(code), spool_flipped_ccfr_figs, .progress="text")
+
+
+
 # Contour plot for many countries, from 1950 onwards, with 2.1 contour highlighted
 
 
@@ -293,3 +366,75 @@ print(shading + replace_line + near_line)
 
 dev.off()
 
+
+
+# flipped lattice plot ----------------------------------------------------
+
+
+
+
+shading <- dta %>% 
+  filter(year >= 1950) %>% 
+  filter( age <= 50 ) %>% 
+  filter(!(code %in% c("DEUTNP", "GBR_NP"))) %>% 
+  levelplot(
+    my_asfr ~ age * birth_year | code, 
+    data=. , 
+    par.strip.text=list(cex=1.4, fontface="bold"),
+    ylab=list(label="Age in years", cex=1.4),
+    xlab=list(label="Birth year", cex=1.4),
+    cex=1.4,
+    cuts=20,
+    col.regions=colorRampPalette(brewer.pal(6, "Purples"))(200),
+    labels=list(cex=1.2),
+    col="black",
+    scales=list(
+      x=list(cex=1.4), 
+      y=list(cex=1.4),
+      alternating=3
+    ),
+    par.settings=list(strip.background=list(col="lightgrey"))
+  ) 
+
+replace_line <- dta %>% 
+  filter(year >= 1950) %>% 
+  filter( age <= 50 ) %>% 
+  filter(!(code %in% c("DEUTNP", "GBR_NP"))) %>% 
+  filter(series_ok == TRUE) %>% 
+  contourplot(
+    my_ccfr ~ age * birth_year | code, 
+    data=. , 
+    region = F,
+    ylab = "",
+    xlab = "",
+    scales = list(NULL),
+    at = c(1.5, 2.05), 
+    lwd = 2, 
+    labels = F
+  )
+
+near_line <- dta %>% 
+  filter(year >= 1950) %>% 
+  filter( age <= 50 ) %>% 
+  filter(!(code %in% c("DEUTNP", "GBR_NP"))) %>% 
+  filter(series_ok == TRUE) %>% 
+  contourplot(
+    my_ccfr ~ age * birth_year  | code, 
+    data=. , 
+    region = F,
+    ylab = "",
+    xlab = "", 
+    scales = list(NULL),
+    at = 1.80, 
+    lwd = 1,
+    lty= "dashed",
+    labels = F
+  )
+
+
+png("figures/flipped_ccfr/latticeplot.png",
+    res=300, width=30, height=30, units = "cm"
+)
+print(shading + replace_line + near_line)
+
+dev.off()
